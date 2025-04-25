5e4bfc0b26874ab286c73fc27de81d47ee928915
TmRAaEx <tmr.aaex@gmail.com>
2025-04-25 22:25:19 +0200

    Removed remnant placeholder files from project creation

--- e-commerce-api/src/interfaces/ITodo.ts
+++
@@ -1,9 +0,0 @@
-export interface ITodo {
-  _id?: string;
-  id?: string;
-  task: string;
-  completed: boolean;
-  createdAt: Date;
-  updatedAt: Date;
-}
-//todo interface that works with mongodb and mysql. If you are using mongoDB you can remove the id field and if you are using mysql you can remove  _id

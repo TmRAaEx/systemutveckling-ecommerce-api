3f63160937ba90567e33a904266bfc7886b4912e
TmRAaEx <tmr.aaex@gmail.com>
2025-04-25 11:45:00 +0200

    added middleware for errpr handling in the router index file

--- e-commerce-api/src/routes/index.ts
+++ e-commerce-api/src/routes/index.ts
@@ -1,11 +1,15 @@
-//base file for routes
-
 import { Router } from "express";
 import productRouter from "./productRoutes";
 import userRouter from "./userRoutes";
+
 export const router = Router();
+
+
 
 router.use("/products", productRouter);
 router.use("/users", userRouter);
 
-//import other routes
+// Fallback for undefined routes
+router.use((req, res) => {
+  res.status(404).json({ error: "There is no api endpoint at this route" });
+});

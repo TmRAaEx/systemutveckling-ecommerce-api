9c0a9b047da4a26f83878d77f8d8eb170fece76f
TmRAaEx <tmr.aaex@gmail.com>
2025-04-25 11:29:18 +0200

    added categories to collections

--- e-commerce-api/src/config/database.ts
+++ e-commerce-api/src/config/database.ts
@@ -13,7 +13,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export type myCollections = {
-  collectionName: "products" | "users" | "carts" | "orders";
+  collectionName: "products" | "users" | "carts" | "orders" | "categories";
 };
 
 /**
